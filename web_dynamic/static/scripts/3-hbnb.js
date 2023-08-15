e519f0c39d35591eda4311bd907460908da1e252
Stephaine Biggs <78326908+Sbiggs1985@users.noreply.github.com>
2023-08-15 15:16:00 -0500

    Update 3-hbnb.js

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,1 +1,19 @@
-
+$(document).ready(function () {
+    $.get("http://0.0.0.0:5000/api/v1/status/", function (data, status) {
+        if (status === "OK") {
+            $("div#api_status").addClass("available");
+        } else {
+            $("div#api_status").removeClass("available");
+        }
+    });
+    let listOfCheckedAmenities = []
+    $('input').change(function() {
+        const amenityName = $(this).attr("data-name");
+        if (this.checked) {
+            listOfCheckedAmenities.push(amenityName);
+        } else {
+            listOfCheckedAmenities = listOfCheckedAmenities.filter((item) => item !== amenityName);
+        }
+        $('div.amenities h4').text(listOfCheckedAmenities.join(', '));
+    });
+});
